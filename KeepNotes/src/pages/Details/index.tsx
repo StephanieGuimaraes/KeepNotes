7a32bf24ffae1d7b5ad62999c21f66ca58b54d62
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-04-19 17:02:26 -0300

    fix: bug in the header

--- KeepNotes/src/pages/Details/index.tsx
+++ KeepNotes/src/pages/Details/index.tsx
@@ -3,6 +3,7 @@ import { Container } from './styles';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 
+
 export function Details() {
   return (
     <Container>
