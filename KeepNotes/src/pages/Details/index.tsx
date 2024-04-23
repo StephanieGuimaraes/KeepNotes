5e80c8a0909de3ebf67dceca306e86315c4686e2
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-04-22 21:10:17 -0300

    feat: using the section component

--- KeepNotes/src/pages/Details/index.tsx
+++ KeepNotes/src/pages/Details/index.tsx
@@ -1,14 +1,24 @@
 
-import { Container } from './styles';
+import { Container, Links } from './styles';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
+import { Section } from '../../components/Section';
 
 
 export function Details() {
   return (
     <Container>
       <Header/>
-      <Header/>
+      <Section title="Links úteis">
+        <Links>
+          <li>
+            <a href='https://github.com/StephanieGuimaraes'>Link 1</a>
+          </li>
+          <li>Item 2</li>
+          <li>Item 3</li>
+        </Links>
+      </Section>
+     
       <Button title="Entrar" loading = {false}/>
     </Container>
   );
