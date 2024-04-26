c863f5cff5fd3bac4cb0b3bab20306c5ab1fd3e9
StephanieGuimaraes <stephanieguimaraes7@gmail.com>
2024-04-26 16:40:01 -0300

    feat: appling the grid logic

--- KeepNotes/src/pages/Details/index.tsx
+++ KeepNotes/src/pages/Details/index.tsx
@@ -1,5 +1,5 @@
 
-import { Container, Links } from './styles';
+import { Container, Links, Content } from './styles';
 import { Button } from '../../components/Button';
 import { Header } from '../../components/Header';
 import { Section } from '../../components/Section';
@@ -11,26 +11,43 @@ export function Details() {
   return (
     <Container>
       <Header/>
-      <ButtonText title="excluir nota"/>
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href='https://github.com/StephanieGuimaraes'>Link 1</a>
-          </li>
-          <li>
-            <a href='https://github.com/StephanieGuimaraes'>Link 2</a>
-          </li>
-          <li>
-            <a href='#'>Item 3</a>
-          </li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
+          <ButtonText title="excluir nota"/>
+          <h1>
+            Introdução ao React
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quae alias dolorem delectus eligendi, dolores iure amet 
+            iste asperiores maiores explicabo quidem numquam quod 
+            quos fuga, obcaecati veniam sint unde ab!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Quae alias dolorem delectus eligendi, dolores iure amet 
+            iste asperiores maiores explicabo quidem numquam quod 
+            quos fuga, obcaecati veniam sint unde ab!
+          </p>
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href='https://github.com/StephanieGuimaraes'>Link 1</a>
+              </li>
+              <li>
+                <a href='https://github.com/StephanieGuimaraes'>Link 2</a>
+              </li>
+              <li>
+                <a href='#'>Item 3</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Marcadores">
-          <Tag title="Express"/>
-      </Section>
-     
-      <Button title="Entrar" loading = {false}/>
+          <Section title="Marcadores">
+              <Tag title="Express"/>
+          </Section>
+        
+          <Button title="Entrar" loading = {false}/>
+      </Content>
+      </main>
     </Container>
   );
 }
